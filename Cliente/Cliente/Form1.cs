26cd66f63f1675e24168b299bf54b6f211b026ef
wenjie <wenjie.sab@gmail.com>
2024-03-10 21:07:58 +0100

    Cliente funcional al logear al servidor.

--- Cliente/Cliente/Form1.cs
+++ Cliente/Cliente/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Cliente
 {
     public partial class Form1 : Form
     {
+        Socket server;
+        
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +23,42 @@ namespace Cliente
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            if(nombre_tb.Text == String.Empty || password_tb.Text == String.Empty)
+            {
+                MessageBox.Show("El campo de nombre y de la contraseña no pueden estar vacias");
+                return;
+            }
+
+            IPAddress direc = IPAddress.Parse("192.168.56.102");
+            IPEndPoint ipep = new IPEndPoint(direc, 9050);
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                server.Connect(ipep);
+                string mensaje = $"2/{nombre_tb.Text}/{password_tb.Text}";
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+                server.Send(msg);
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
+                MessageBox.Show("Tu id de jugador es: " + mensaje);
+
+            }catch(SocketException err)
+            {
+                MessageBox.Show("Error: " + err.Message);
+            }
+            finally
+            {
+                server.Shutdown(SocketShutdown.Both);
+                server.Close();
+            }
 
         }
     }
