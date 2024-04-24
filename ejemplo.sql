9216bb35fabb5ec1614a9219d6ffe91d60d64f9b
wenjie <wenjie.sab@gmail.com>
2024-04-24 17:58:25 +0200

    He arreglado la base de datos.

--- ejemplo.sql
+++ ejemplo.sql
@@ -1,7 +1,7 @@
 use juego;
 insert into Jugador (Nombre, pass) values ('Juan','1234');
-insert into Chat (id, mensajes, fecha) values (7, "Hola", NOW())
+insert into Chat (id, mensajes, fecha) values (7, "Hola", NOW());
 insert into Partida (id, chat_id) values (23, 7);
 insert into Nucleo (id_j, id_p) values (1,23);
-insert into Jugador (Nombre, id, pass) values ('Pepe',2,'qwerty');
+insert into Jugador (Nombre, pass) values ('Pepe','qwerty');
 insert into Nucleo (id_j, id_p) values (2,23);
